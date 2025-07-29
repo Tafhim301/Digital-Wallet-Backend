8f8d568ce30bfde1a9457dad282a757559dee9f0
Tafhim301 <tafhim301@gmail.com>
2025-07-29 20:18:10 +0600

    Added app and servie files

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express'
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Digital Wallet Backend",
+  });
+});
+
+
+export default app;
